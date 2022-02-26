bb7adc7cf5b09900a88d5c9ab4288f26b567cbce
Arsanyos <arsanyos1600@gmail.com>
2022-02-26 13:16:48 +0300

    navbar design

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,12 @@ import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 //importing style
 import "../style/App.css";
+//importing components
+import NavBar from "./NavBar.jsx";
 function App() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function NavBar() {
+  return (
+    <div className="nav-bar-container">
+      <nav className="navbar navbar-expand-lg ">
+        
+      </nav>
+    </div>
+  );
+}
+export default NavBar;
