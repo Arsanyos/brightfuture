59c61aa859723e3b4d2157e7166323f11341a622
Arsanyos <arsanyos1600@gmail.com>
2022-02-26 11:41:21 +0300

    reponsive setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-function App(){
-    return(
-        
-    );
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+//importing style
+import "../style/App.css";
+function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
 }
 export default App;
